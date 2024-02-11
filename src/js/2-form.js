cc1a5a35e18a14e1d423acf31c4a0382e7694bb9
Nadiia1701 <146233511+Nadiia1701@users.noreply.github.com>
2024-02-11 05:55:31 -0500

    SUMMARY

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,3 +1,45 @@
 "use strict"
 
+const form = document.querySelector(".feedback-form");
+const localStorageKey = "feedback-form-state";
 
+function readFormData(form) {
+    const message = form.message.value;
+    const email = form.email.value;
+    return {
+        message,
+        email,
+    };
+}
+
+form.addEventListener("input", (event) => {
+    event.preventDefault();
+    const data = readFormData(event.currentTarget);
+    const jsonData = JSON.stringify(data);
+    localStorage.setItem(localStorageKey, jsonData);
+});
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    localStorage.removeItem(localStorageKey);
+    form.reset();
+});
+
+
+
+
+
+
+
+
+
+
+// const localData = JSON.parse(localStorage.getItem(localStorageKey));
+// console.log(localData);
+
+// const rawData = localStorage.getItem(localStorageKey);
+// if (rawData){
+//     const data = JSON.parse(rawData);
+//     form.email.value = data.email;
+//     form.message.value = data.message;
+// }
