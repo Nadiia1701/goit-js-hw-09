b2075f05cea6ea7afb4b8a23524c504441cd0e7a
Nadiia1701 <146233511+Nadiia1701@users.noreply.github.com>
2024-02-11 07:09:10 -0500

    SUMMARY2

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,11 +1,14 @@
 "use strict"
 
+
 const form = document.querySelector(".feedback-form");
 const localStorageKey = "feedback-form-state";
 
+// Використовуючи делегуваня, відстежуй на формі подію input і щоразу записуй у локальне сховище об'єкт з полями email і message, у яких зберігай поточні значення полів форми. Нехай ключем для сховища буде рядок "feedback-form-state".
+
 function readFormData(form) {
-    const message = form.message.value;
     const email = form.email.value;
+    const message = form.message.value;
     return {
         message,
         email,
@@ -19,27 +22,24 @@ form.addEventListener("input", (event) => {
     localStorage.setItem(localStorageKey, jsonData);
 });
 
+// Під час завантаження сторінки перевіряй стан сховища, і якщо там є збережені дані, то заповнюй ними поля форми. В іншому випадку поля повинні бути порожніми.
+
+const localData = localStorage.getItem(localStorageKey);
+if (localData){
+    const data = JSON.parse(localData);
+    form.email.value = data.email;
+    form.message.value = data.message;
+}
+
+// Під час сабміту форми очищай сховище і поля форми, а також виводь у консоль об'єкт з полями email, message та їхніми поточними значеннями.
+
 form.addEventListener("submit", (event) => {
     event.preventDefault();
+    const object = {
+    email: event.currentTarget.elements.email.value,
+    message: event.currentTarget.elements.message.value,
+    };
+    console.log(object);
     localStorage.removeItem(localStorageKey);
     form.reset();
 });
-
-
-
-
-
-
-
-
-
-
-// const localData = JSON.parse(localStorage.getItem(localStorageKey));
-// console.log(localData);
-
-// const rawData = localStorage.getItem(localStorageKey);
-// if (rawData){
-//     const data = JSON.parse(rawData);
-//     form.email.value = data.email;
-//     form.message.value = data.message;
-// }
